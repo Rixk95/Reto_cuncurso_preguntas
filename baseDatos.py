93514d207f43cf25a1221d425ebb48b57d7fd348
Rixk95 <jooljodi@gmail.com>
2021-09-11 13:19:48 -0500

    claseBase y claseJugador

---
+++ baseDatos.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+class Base:
+    def __init__(self,nombre,puntaje):
+        self.nombre=nombre
+        self.puntaje=puntaje
+
+    def identifica(self):
+        miConexion=sqlite3.connect("baseDatos")
+        miCursor=miConexion.cursor()
+        miCursor.execute("SELECT * FROM PUNTUACION")
+        score=miCursor.fetchall()
+        miConexion.commit()
+        miConexion.close()
+        if len(score)==0:
+            return "VACIO"
+    def evalua(self):
+        miConexion=sqlite3.connect("baseDatos")
+        miCursor=miConexion.cursor()
+        miCursor.execute("SELECT * FROM PUNTUACION")
+        score=miCursor.fetchall()
+        miConexion.commit()
+        miConexion.close()
+        if self.identifica()=="VACIO":
+            self.guardarDato(self.puntaje)
+        else:
+            bandera=0
+            for registro in score:
+                if registro[0]==self.nombre:
+                    bandera=1
+            if bandera==0:
+                self.guardarDato(self.puntaje)
+            elif bandera==1:
+                if self.puntaje>self.consultaVictorias():
+                    self.actualizaDato(self.puntaje)             
+            
+    def guardarDato(self,puntaje):
+        miConexion=sqlite3.connect("baseDatos")
+        miCursor=miConexion.cursor()
+        miCursor.execute(f'INSERT INTO PUNTUACION VALUES("{self.nombre}",{self.puntaje})')
+        miConexion.commit()
+        miConexion.close()
+    def actualizaDato(self,nuevoValor):
+        miConexion=sqlite3.connect("baseDatos")
+        miCursor=miConexion.cursor()
+        miCursor.execute(f'UPDATE PUNTUACION SET PUNTAJE={nuevoValor} WHERE NOMBRE="{self.nombre}"')
+        miConexion.commit()
+        miConexion.close()
+    def consultaVictorias(self):
+        miConexion=sqlite3.connect("baseDatos")
+        miCursor=miConexion.cursor()
+        miCursor.execute(f'SELECT PUNTAJE FROM PUNTUACION WHERE NOMBRE="{self.nombre}"')
+        anticipo=miCursor.fetchall()
+        for victorias in anticipo:
+            valorVictorias=victorias[0]
+        miConexion.commit()
+        miConexion.close()
+        return valorVictorias
+    def mostrarScore(self):
+        miConexion=sqlite3.connect("baseDatos")
+        miCursor=miConexion.cursor()
+        miCursor.execute(f'SELECT * FROM PUNTUACION ORDER BY PUNTAJE DESC')
+        tablaScore=miCursor.fetchall()
+        miConexion.commit()
+        miConexion.close()
+        print("----------SCORE----------")
+        num=1
+        for x in tablaScore:
+            print(f'Puesto No.{num} ... {x[0]} ... Puntaje: {x[1]}')
+            num+=1
---
+++ jugador.py
@@ -0,0 +1,60 @@
+from ronda import Ronda
+from categoria import Categoria
+import random
+from pregunta import Pregunta
+from opciones import Opciones
+from evaluador import Evaluador
+from premio import Premio
+from baseDatos import Base
+
+print('''
+Bienvenido al juego "Pon a prueba tus matematicas"
+Juego dirigido a niños
+Consta de 5 rondas con niveles de dificultades ascendentes,
+las preguntas pueden variar y cada una tiene 4 opciones de 
+respuesta con unica respuesta correcta, tambien contiene una
+opcion de rendirse.
+Si al finalizar el juego sacas un puntaje inferior al que a tenias,
+el SCORE no se actualizara ya que el SCORE guarda tu mejor puntaje.
+Al finalizar se te mostrara el SCORE donde podras ver tu
+mejor puntuacion y compararla con otros competidores.
+Buena suerte!! 
+
+''')
+nombre=input("Ingresa tu nombre:\n").upper()
+for cont in range(5):
+    ronda=Ronda(cont+1)
+    NRonda=ronda.n_ronda()
+    print("--"*20)
+    print("Ronda numero: "+str(NRonda))
+    categoria=Categoria(NRonda)
+    seccion=categoria.nomCategoria()
+    print("Nivel de dificultad: "+seccion)
+    if NRonda==1:
+        nuevoPuntaje=0
+    puntuacion=0+nuevoPuntaje
+    print("Puntaje actual: "+str(puntuacion))
+    premio=Premio(seccion,puntuacion)
+    numAleatorio=random.randint(0,4)
+    pregunta=Pregunta(seccion,numAleatorio)
+    cuestion=pregunta.preguntaAleatoria()
+    print("Pregunta:   "+cuestion)
+    opciones=Opciones(seccion,numAleatorio)
+    posibilidades=opciones.opcionesPregunta()
+    print(posibilidades)
+    eleccion=input("Elige la opcion correcta:\n").upper()
+    evaluador=Evaluador(seccion,numAleatorio,eleccion)
+    resultado=evaluador.evaluar()
+    if resultado==1:
+        nuevoPuntaje=premio.valorar()
+    else:
+        print("\nFIN DEL JUEGO\nTU PUNTAJE ES DE "+str(puntuacion)+" Y SERA GUARDADA EN EL SCORE.\n")
+        base=Base(nombre,puntuacion)
+        base.evalua()
+        base.mostrarScore()
+        exit(0)
+puntuacion=1500
+print("FELICITACIONES, SUPERASTE TODAS LAS RONDAS.\nTU PUNTAJE ES DE "+str(puntuacion)+" Y QUEDARA GUARDADO EN EL SCORE.\nGRACIAS POR JUGAR.\n")
+base=Base(nombre,puntuacion)
+base.evalua()
+base.mostrarScore()
